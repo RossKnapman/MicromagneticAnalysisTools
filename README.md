b208cb52d4db88510cce8ce7034d3022f44d1cde
RossKnapman <rjknapman@gmail.com>
2021-09-02 12:20:57 +0100

    Write setup.py

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Code I use for analysing data from MuMax3.
 
 - [x] Write unit tests
 - [x] Refactor code so that code with the same functionality is not duplicated
-- [ ] Write `setup.py`
+- [x] Write `setup.py`
 - [ ] Delete redundant functions e.g. COM calculation that does not go to a comoving frame
 - [ ] Some functions (e.g. `vecToRGB`) should be moved to a separate `utils.py` directory
 - [ ] 'Proper' docstrings
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt', 'r') as f:
+    install_requires = f.readlines()
+
+setup(
+    name="MicromagneticAnalysisTools",
+    version="0.0.1",
+    description="A collection of tools to analyse data from MuMax3.",
+    author="Ross Knapman",
+    author_email="rjknapman@gmail.com",
+    packages=find_packages(include=['src', 'src/*']),
+    install_requires=install_requires,
+)
